14913dd38a153e60ad8aaab40fd7825f3e5f6dbb
Omer M <omerminhas1@gmail.com>
2023-06-18 19:20:13 +0100

    added cors proxy

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -53,7 +53,7 @@ const Customizer = () => {
     try {
       setGeneratingImg(true);
 
-      const response = await fetch('https://threed-clothing-demo.onrender.com/api/v1/dalle', {
+      const response = await fetch('https://thingproxy.freeboard.io/fetch/https://threed-clothing-demo.onrender.com/api/v1/dalle', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'

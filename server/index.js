aa33613f04937c8e4d4c088a5565155e329f2048
Omer M <omerminhas1@gmail.com>
2023-06-18 21:17:58 +0100

    edited cors

--- server/index.js
+++ server/index.js
@@ -6,15 +6,9 @@ import dalleRoutes from './routes/dalle.routes.js';
 
 dotenv.config();
 
-const corsOptions ={
-  origin:'https://3d-clothing-demo-eexw.vercel.app', 
-  credentials:true,            //access-control-allow-credentials:true
-  optionSuccessStatus:200,
-}
-
 
 const app = express();
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json({ limit: "50mb" }))
 
 app.use("/api/v1/dalle", dalleRoutes);
